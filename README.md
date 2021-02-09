7b5b5cec8c8d195319128b5fdab4362648445156
tfuller <tmfuller49589@gmail.com>
2021-02-08 22:47:27 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Project batch created and pushed to GitLab by T.Fuller
---
+++ src/main/java/foobar/FooBar.java
@@ -0,0 +1,13 @@
+package foobar;
+
+// Project batch created from by T. Fuller
+// and batch uploaded to GitHub
+// scripts: gitlabRepoMaker gitlabRepoMakerBatch
+
+public class FooBar {
+
+	public static void main(String[] args) {
+		
+		System.out.println("this is a FooBar");
+	}
+}
