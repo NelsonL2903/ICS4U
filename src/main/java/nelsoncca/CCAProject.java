cc0573dcf08909b0c391f4badcd1a91b2f63fe57
LoopNels162 <LoopNels162@2060I-L207-16.tvnet.Pri>
2021-02-24 13:04:43 -0500

    turning nodes

--- src/main/java/nelsoncca/CCAProject.java
+++ src/main/java/nelsoncca/CCAProject.java
@@ -11,9 +11,6 @@ import com.jme3.scene.shape.Box;
 
 public class CCAProject extends SimpleApplication {
 	
-	int x = 0;
-	int y = 3;
-	int z = 0;
 	Geometry barrel;
 	Geometry handle;
 	Node playerNode = new Node("Player Node");
@@ -26,24 +23,26 @@ public class CCAProject extends SimpleApplication {
 	@Override
 	public void simpleInitApp() {
 		
-		Box a = new Box(new Vector3f(0,0,0), 5, 1, 5);
+		Box a = new Box(new Vector3f(0,-5,0), 10, 0.5f, 10);
 		Geometry floor = new Geometry("Box", a);
 	    Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
 		mat.setColor("Color", ColorRGBA.White);
 		floor.setMaterial(mat);
 		rootNode.attachChild(floor);
+		
 	
-		this.cam.setLocation(rootNode.localToWorld(new Vector3f(x+6, y+20, z-20), null));
+		this.cam.setLocation(rootNode.localToWorld(new Vector3f(6, 23, -20), null));
+		playerNode.setLocalTranslation(cam.getLocation());
 		getCamera().lookAt(Vector3f.ZERO, Vector3f.UNIT_Y);
 		
-		Box b = new Box(new Vector3f(0,3,0), 1, 2, 1);
+		Box b = new Box(new Vector3f(-15,-27,21), 1, 2, 1);
 		handle = new Geometry("Box", b);
 		Material mat2 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
 		mat2.setColor("Color", ColorRGBA.Gray);
 		handle.setMaterial(mat2);
 		playerNode.attachChild(handle);
 		
-		Box c = new Box(new Vector3f(0,5,2), 1, 1, 4);
+		Box c = new Box(new Vector3f(-15,-25,23), 1, 1, 4);
 		barrel = new Geometry("Box", c);
 		Material mat3 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
 		mat3.setColor("Color", ColorRGBA.LightGray);
@@ -52,14 +51,17 @@ public class CCAProject extends SimpleApplication {
 		rootNode.attachChild(playerNode);
 		playerNode.attachChild(barrel);
 		
-		playerNode.setLocalTranslation(0,5,0);
-		
 	}
 	
 	public void simpleUpdate(float tpf) {
 		
+		playerNode.setLocalRotation(cam.getRotation());
+		barrel.setLocalTranslation(cam.getLocation().getX()-15,cam.getLocation().getY()-25,cam.getLocation().getZ()+21);
+		handle.setLocalTranslation(cam.getLocation().getX()-15,cam.getLocation().getY()-26,cam.getLocation().getZ()+21);
 		
-	
+		System.out.println(cam.getRotation());
+		System.out.println(playerNode.getLocalRotation());
+		
 	}
 
 }
