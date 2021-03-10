0ca731b68d61d51924dbd8674e71464572798957
Nelson <Nelson@10.0.0.242>
2021-03-10 18:45:07 -0500

    selection sort done

--- src/main/java/sorting/SelectionSort.java
+++ src/main/java/sorting/SelectionSort.java
@@ -2,6 +2,11 @@ package sorting;
 
 import java.util.Random;
 
+// A selection sort works by determining the lowest value in an array and then swapping
+// the first value in the array with the smallest value in the array so that the smallest
+// value is at the start of the array. This process is then repeated but now excluding the
+// first value of the array. This process is continually repeated until all of the numbers
+// in the array are in numerical order.
 public class SelectionSort {
 
 public static void main(String[] args) {
@@ -38,7 +43,10 @@ private void sort(int[] array) {
 	for (int i = 0; i < array.length-1; ++i) {
 		int minidx = i;
 		for (int j = i + 1; j < array.length; ++j) {
-			// 
+			// if the current value of array[j] is less than the value of array[minidx],
+			// than minidx is set equal to the value of j, this process will repeat
+			// until the smallest value of the array has been found, and then the
+			// smallest value will be swapped with the first value in the array
 			if (array[j] < array[minidx]) {
 			minidx = j;
 			}
