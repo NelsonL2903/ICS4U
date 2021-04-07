63f45e0892d6d9df91ecd6e5671e4e0eeb258640
LoopNels162 <LoopNels162@2060I-L207-16.tvnet.Pri>
2021-04-07 10:46:11 -0400

    nifty

---
+++ src/main/java/nelsoncca/NiftyGUI.java
@@ -0,0 +1,38 @@
+package nelsoncca;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.niftygui.NiftyJmeDisplay;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.nifty.screen.ScreenController;
+
+public class NiftyGUI extends SimpleApplication implements ScreenController{
+
+	@Override
+	public void simpleInitApp() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void bind(Nifty nifty, Screen screen) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStartScreen() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onEndScreen() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
