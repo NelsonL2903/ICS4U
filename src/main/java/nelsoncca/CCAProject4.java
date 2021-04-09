4bc0db47e9ea84e2a70021b197be12dce1bdd5af
LoopNels162 <LoopNels162@2060I-L207-16.tvnet.Pri>
2021-04-09 09:10:07 -0400

    nifty

--- src/main/java/nelsoncca/CCAProject4.java
+++ src/main/java/nelsoncca/CCAProject4.java
@@ -1,44 +1,69 @@
 package nelsoncca;
 
 import com.jme3.app.SimpleApplication;
-import com.jme3.material.Material;
 import com.jme3.niftygui.NiftyJmeDisplay;
-import com.jme3.scene.Geometry;
-import com.jme3.scene.shape.Box;
 import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.controls.TextField;
 import de.lessvoid.nifty.screen.Screen;
 import de.lessvoid.nifty.screen.ScreenController;
+import nelsoncca.CCAProject4;
 
 public class CCAProject4 extends SimpleApplication implements ScreenController {
 
+	private Nifty nifty;
+	CCAProject5 cca5 = new CCAProject5();
+	
 	public static void main(String[] args) {
-
-		CCAProject5 cca5 = new CCAProject5();
-		cca5.starter(args);
+		
+		CCAProject4 app = new CCAProject4();
+		app.setPauseOnLostFocus(false);
+		app.start();
+		
 	}
 
 	@Override
 	public void bind(Nifty nifty, Screen screen) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("bind( " + screen.getScreenId() + ")");
 	}
 
 	@Override
 	public void onStartScreen() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("onStartScreen");
 	}
 
 	@Override
 	public void onEndScreen() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("onEndScreen");
+	}
+	
+	public void quit() {
+		System.out.println("quit");
 	}
 
 	@Override
 	public void simpleInitApp() {
-		// TODO Auto-generated method stub
+
+		NiftyJmeDisplay niftyDisplay = NiftyJmeDisplay.newNiftyJmeDisplay(assetManager, inputManager, audioRenderer,
+				guiViewPort);
+		nifty = niftyDisplay.getNifty();
+		nifty.fromXml("FinalProject.xml", "start", this);
+
+		guiViewPort.addProcessor(niftyDisplay);
+
+		flyCam.setEnabled(false);
+		flyCam.setDragToRotate(true);
+		inputManager.setCursorVisible(true);
 		
+	}
+	
+	public void testMethod2() {
+		String s = nifty.getCurrentScreen().findNiftyControl("nametextfield", TextField.class).getDisplayedText();
+		System.out.println("test method test field, displayed text is " + s);
+		cca5.starter(s);
+	}
+	
+	public void testMethodbuttonCancel() {
+		System.exit(0);
 	}
 
 }
